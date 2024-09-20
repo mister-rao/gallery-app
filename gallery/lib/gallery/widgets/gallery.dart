ae75595b95f2856cb28e30ab91aaeff2b0b9489e
gprao <mister.xrao@gmail.com>
2024-09-20 18:19:19 +0530

    added basic paginator

--- gallery/lib/gallery/widgets/gallery.dart
+++ gallery/lib/gallery/widgets/gallery.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gallery/app/permission_handler.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:photo_manager/photo_manager.dart';
 
 class GalleryPage extends StatefulWidget {
@@ -11,12 +12,41 @@ class GalleryPage extends StatefulWidget {
 
 class _GalleryPageState extends State<GalleryPage> {
   final permissions = PermissionHandler();
+  static const _pageSize = 10;
 
+  final _pagingController = PagingController<int, String>(firstPageKey: 0);
   @override
   void initState() {
+    _pagingController.addPageRequestListener(_fetchPage);
     permissions.checkPhotosPermission();
     getPhotos();
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      final images = [
+        'Image_1',
+        'Image_2',
+        'Image_3',
+        'Image_4',
+      ];
+      final isLastPage = images.length < _pageSize;
+      if (isLastPage) {
+        _pagingController.appendLastPage(images);
+      } else {
+        final nextPageKey = pageKey + images.length;
+        _pagingController.appendPage(images, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
+    }
   }
 
   Future<void> getPhotos() async {
@@ -40,13 +70,17 @@ class _GalleryPageState extends State<GalleryPage> {
           )
         ],
       ),
-      body: GridView.builder(
-        itemCount: 10,
+      body: PagedGridView(
+        pagingController: _pagingController,
         gridDelegate:
             const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        itemBuilder: (context, index) => Container(
-          margin: const EdgeInsets.all(1),
-          color: Colors.grey,
+        builderDelegate: PagedChildBuilderDelegate(
+          itemBuilder: (context, item, index) {
+            return Container(
+              margin: const EdgeInsets.all(1),
+              color: Colors.grey,
+            );
+          },
         ),
       ),
     );
